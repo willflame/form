9ecfeef1e0a3cb42737901bc29053dc6a87e0ce1
Willian Anselmo da Silva <willian0506@hotmail.com>
2020-12-24 14:10:59 -0300

    Formulários reativos: Radio Button (Botão do tipo Rádio)

--- src/app/data-form/data-form.component.ts
+++ src/app/data-form/data-form.component.ts
@@ -19,6 +19,7 @@ export class DataFormComponent implements OnInit {
   estados: Observable<EstadoBr[]>;
   cargos: any[];
   tecnologias: any[];
+  newsletterOp: any[];
 
   constructor(
     private formBuilder: FormBuilder,
@@ -33,8 +34,10 @@ export class DataFormComponent implements OnInit {
     this.estados = this.dropDownService.getEstadosBr();
 
     this.cargos = this.dropDownService.getCargos();
+
     this.tecnologias = this.dropDownService.getTecnologias();
 
+    this.newsletterOp = this.dropDownService.getNewsletter();
 
     // this.formulario = new FormGroup({
     //   nome: new FormControl(null),
@@ -59,7 +62,8 @@ export class DataFormComponent implements OnInit {
       }),
 
       cargo: [null],
-      tecnologia: [null]
+      tecnologia: [null],
+      newsletter: ['s']
     });
 
   }
--- src/app/shared/services/dropdown.service.ts
+++ src/app/shared/services/dropdown.service.ts
@@ -28,4 +28,11 @@ export class DropdownService {
             { nome: 'ruby', desc: 'Ruby' }
         ];
     }
+
+    getNewsletter() {
+        return [
+            { valor: 's', desc: 'Sim' },
+            { valor: 'n', desc: 'Não' }
+        ];
+    }
 }
